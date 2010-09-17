3b21bd0835da98b79f57de547bc6fd7634989acc
Matthew Stickney <fiendishlinux@gmail.com>
2010-09-16 20:07:46 -0400

    Added get_name()

--- sort.c
+++ sort.c
@@ -1,5 +1,37 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #include "pipe_util.h"
+#include "stralloc.h"
+
+char *get_name(FILE *f)
+{
+	int c;
+	stralloc name = {0};
+	char *strp;
+	char d;
+
+	/* consume leading whitespace */
+	do {
+		c = fgetc(f);	
+	} while(isspace(c));
+	ungetc(c, f);
+
+	/* append non-whitespace chars */
+	while (1) {
+		c = fgetc(f);
+		if (c < 0 || isspace(c)) {
+			stralloc_0(&name);
+			strp = strdup(name.s);
+			stralloc_free(&name);
+			return strp;
+		}
+		d = c;
+		stralloc_append(&name, &d);
+	}
+}
+
 
 int main(int argc, char **argv)
 {
